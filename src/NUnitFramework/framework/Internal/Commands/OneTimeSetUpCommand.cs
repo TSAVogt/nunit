e882f814efbe2b24f8609e32aa9cc3d4beb4f0bc
Stefan Vogt <stefan.sv.vogt@siemens-healthineers.com>
2024-03-28 16:26:24 +0100

    Callbacks with methods info implemented

--- src/NUnitFramework/framework/Internal/Commands/OneTimeSetUpCommand.cs
+++ src/NUnitFramework/framework/Internal/Commands/OneTimeSetUpCommand.cs
@@ -1,7 +1,5 @@
 // Copyright (c) Charlie Poole, Rob Prouse and Contributors. MIT License - see LICENSE.txt
 
-using Testing.sdk;
-
 namespace NUnit.Framework.Internal.Commands
 {
     /// <summary>
@@ -23,17 +21,7 @@ namespace NUnit.Framework.Internal.Commands
 
             BeforeTest = context =>
             {
-                try
-                {
-                    if (TestContext.Parameters.Names.Contains("RuntimeCallbacks"))
-                        TestLog.Log($"- BeforeOneTimeSetUp");
-                    setUpTearDown.RunSetUp(context);
-                }
-                finally
-                {
-                    if (TestContext.Parameters.Names.Contains("RuntimeCallbacks"))
-                        TestLog.Log($"- AfterOneTimeSetUp");
-                }
+                setUpTearDown.RunSetUp(context);
             };
         }
     }
